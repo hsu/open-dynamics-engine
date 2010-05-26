24cd07c56b042f789deadcaa02efc51f94207ee1
oleh_derevenko <oleh_derevenko>
2010-05-26 11:30:40 +0000

    Cosmetic: Compilation warning fixed

--- include/ou/assert.h
+++ include/ou/assert.h
@@ -152,7 +152,7 @@
 	|| (!_OU_NAMESPACE::CAssertionCheckCustomization::GetAssertFailureCustomHandler() \
 		|| (_OU_NAMESPACE::CAssertionCheckCustomization::GetAssertFailureCustomHandler()( \
 			_OU_NAMESPACE::AFS_CHECK,  #Condition, __FILE__, __LINE__), false)) \
-		|| (*(int *)0 = 0) != 0)
+		|| (*(int *)0 = 0, false))
 
 
 #else // #if !defined(NDEBUG)
@@ -165,7 +165,7 @@
 		&& (_OU_NAMESPACE::CAssertionCheckCustomization::GetAssertFailureCustomHandler()( \
 			_OU_NAMESPACE::AFS_ASSERT, #Condition, __FILE__, __LINE__), true)))
 
-#define OU__CHECK_HANDLER(Condition) ((bConditionValue = false || (Condition)) \
+#define OU__CHECK_HANDLER(Condition) (((bConditionValue = false, false) || (Condition)) \
 	|| (_OU_NAMESPACE::CAssertionCheckCustomization::GetAssertFailureCustomHandler() \
 		&& (_OU_NAMESPACE::CAssertionCheckCustomization::GetAssertFailureCustomHandler()( \
 			_OU_NAMESPACE::AFS_CHECK,  #Condition, __FILE__, __LINE__), true)))
@@ -178,7 +178,7 @@
 #define OU_CHECK(Condition) { \
 	bool bConditionValue; \
 	assert(OU__CHECK_HANDLER(Condition)); \
-	(void)(bConditionValue || (*(int *)0 = 0) != 0); \
+	(void)(bConditionValue || (*(int *)0 = 0, false)); \
 }
 
 
